38ec6236abf80bbe9e8357dad41a0302fd39bb42
Robert Bonglamphone <8757841+bonglrob@users.noreply.github.com>
2025-02-21 15:58:05 -0800

    fix(removeFromArray): pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(givenArray, ...toRemove) {
+    return givenArray.filter((elem) => !toRemove.includes(elem));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
