9b0c530f68b6ec7018958d491bec961deaba83a3
Robert Bonglamphone <8757841+bonglrob@users.noreply.github.com>
2025-02-21 14:08:26 -0800

    fix(repeatString): pass all tests for repeating a string
    practice with loops and jest testing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numTimesToRepeat) {
+    repeatedString = '';
 
+    if (numTimesToRepeat < 0) { 
+        return 'ERROR'; 
+    } else if (numTimesToRepeat === 0) {
+        return '';
+    } else {
+        for (let i = 0; i < numTimesToRepeat; i++) {
+            repeatedString += stringToRepeat;
+        }
+        return repeatedString
+    }
 };
 
 // Do not edit below this line
