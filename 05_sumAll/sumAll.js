220d6ae9821035096683e859940f365edb7af322
Robert Bonglamphone <8757841+bonglrob@users.noreply.github.com>
2025-02-22 10:51:26 -0800

    fix(sumAll): sum of all numbers and practice with isInteger()

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    if (startNum < 0 || endNum < 0 || !Number.isInteger(startNum) || !Number.isInteger(endNum)) return 'ERROR';
 
+    let startIndex = startNum;
+    let endIndex = endNum;
+
+    if (endIndex < startIndex) {
+        startIndex = endNum;
+        endIndex = startNum;
+    } 
+
+    for (let i = startIndex; i <= endIndex; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
